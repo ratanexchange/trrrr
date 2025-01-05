8a5cbbfcea56bc480a6920106de641538e27e5dc
dinesh <Ratanexchange@gmail.com>
2025-01-05 15:28:13 +0530

    12345

--- js/constants.js
+++
@@ -1,4 +0,0 @@
-export const HOME_PAGE_NAME = 'Home Page';
-export const CAREERS_ROUTE = 'careers';
-export const JOB_DESCRIPTION_ROUTE = 'job-description';
-export const JOB_APPLICATION_ROUTE = 'job-application';
